c229b2756dc58543384a2b0ec9e781244d0a7c19
Aaron-Ccente <aronccente@gmail.com>
2025-12-05 21:58:58 -0500

    delete: validators eliminado

--- utils/ValidatorForm.tsx
+++
@@ -1,6 +0,0 @@
-import { User } from "@/interface/validator";
-
-export const validator = (user: User) =>{
-    if(!user || !user.name.trim()) return 'Ingrese el nombre, no debe estar vacio';
-    return user.name;
-}
